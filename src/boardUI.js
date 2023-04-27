2cfbfc281220ee575fa4991a12ec3f84df346c58
JasonPC <jasonwong287962@gmail.com>
2023-04-27 15:46:13 -0500

    moved Helperfunction to new file

--- src/boardUI.js
+++ src/boardUI.js
@@ -1,10 +1,18 @@
-import { knightMoves, allMoves, getRoot } from "./knight_travails";
-import node from "./node";
-let form = document.querySelector(".formdata");
-const positions = { startPos: [], endPos: [], startPosString: "" };
+import {
+  positions,
+  convertPositiontoString,
+  calculateMoves,
+  resetBoard,
+  createKnightElement,
+  startEndPositionisSame,
+  resetAllPositionMemory,
+  makeRowDiv,
+  setInstructionText,
+  updateHTML,
+} from "./boardUIHelper";
+
 // form.addEventListener("submit", getPositions);
 
-const information = document.querySelector(".information");
 // and add EVENTLISTENER
 function makeBoard() {
   const board = document.querySelector(".board");
@@ -22,9 +30,6 @@ function makeBoard() {
   }
 }
 
-function convertPositiontoString(string, splitter = ",") {
-  return string.split(splitter).map((x) => x * 1);
-}
 //
 
 function setKnightPosition(e) {
@@ -50,67 +55,6 @@ function setKnightPosition(e) {
     resetAllPositionMemory();
     setInstructionText("Choose Starting Position!");
   }
-}
-
-// HELPER FUNCTIONS
-function setInstructionText(instruction) {
-  const instructions = document.querySelector(".instruction");
-  instructions.textContent = instruction;
-}
-function makeRowDiv() {
-  const rowDiv = document.createElement("div");
-  rowDiv.classList.add("boardRow");
-  return rowDiv;
-}
-function resetAllPositionMemory() {
-  positions.startPos = [];
-  positions.endPos = [];
-  positions.startPosString = "";
-}
-function startEndPositionisSame(e) {
-  if (positions.startPosString === e.currentTarget.dataset.position) {
-    return true;
-  }
-}
-
-function createKnightElement() {
-  const knight = document.createElement("img");
-  knight.classList.add("knightImg");
-  knight.classList.add("move");
-  return knight;
-}
-function resetBoard() {
-  const boardElements = document.querySelectorAll(".boardElement");
-  boardElements.forEach((ele) => {
-    ele.textContent = "";
-    // ele.className.add("boardElement");
-  });
-}
-
-function calculateMoves(startPosArray, endPosArray) {
-  return knightMoves(startPosArray, endPosArray);
-}
-
-function updateHTML(array) {
-  let i = 0;
-
-  array.forEach((element) => {
-    i++;
-
-    const boardElement = document.querySelector(`[data-position="${element}"]`);
-
-    const div = document.createElement("div");
-    div.classList.add("moveNumber");
-    const knight = createKnightElement();
-
-    if (i != 1 && i != array.length) {
-      div.textContent = i;
-      boardElement.appendChild(knight);
-    } else {
-      div.textContent = i;
-    }
-    boardElement.appendChild(div);
-  });
 }
 
 export { makeBoard };
